94f5c8c1832e05d8b2d610e89b8276e27e471499
Sebasberacu <sebasberacu@gmail.com>
2022-09-12 13:32:01 -0600

    Hecho pa

--- creacionTablas.sql
+++ creacionTablas.sql
@@ -1,3 +1,9 @@
+/*
+Tarea JSON || Bases de Datos II
+Por: Felipe Obando & Sebastián Bermúdez
+12 de septiembre del 2022
+*/
+
 CREATE TABLE Persona(
     idPersona serial not null primary key,
     infoPersona JSON
@@ -21,9 +27,12 @@ CREATE TABLE Venta(
     productos JSON,
     total integer
 );
-
+CREATE TYPE busquedaProductos AS( --para la búsqueda de productos
+	precioBuscado text,
+	cantidadBuscada int
+);
 /*
---INSERTAR DATOS A PIE
+--INSERTAR DATOS A PIE y LLAMADAS A FUNCIONES
 --Cliente
 INSERT INTO Cliente(infoPersona) VALUES('{
     "nombre": "Felipe",
@@ -82,4 +91,59 @@ Insert into  Producto(idEmpleado,infoProducto,cantidad
 								"fechaFabricacion": "2022-09-08",
 								"precio": 500
 							}',50);
+
+call generarPedido(1,1,'{ 
+				   "nombre": "Helado hechizo 3",
+					"tipoPresentacion": "cono",
+					"sabor":[
+						{"nombre": "cacao"},
+						{"nombre": "guacamole"}
+						] }');
+						  
+
+CALL CompraProducto('{ "productos":[
+							{
+								"nombre": "Helado de Fresa",
+								"tipoPresentacion": "Cono",
+								"sabor":[
+									{"nombre": "Fresa"},
+									{"nombre": "Vainilla"}
+								],
+								"azucar": 100,
+								"numeroLote": 1,
+								"fechaFabricacion": "2022-09-08",
+								"precio": 600,
+								"cantidad":2
+							},
+							{
+								"nombre": "Helado Chocolate",
+								"tipoPresentacion": "Paleta",
+								"sabor":[
+									{"nombre": "Chocolate"},
+									{"nombre": "Oreo"}
+								],
+								"azucar": 500,
+								"numeroLote": 2,
+								"fechaFabricacion": "2022-09-08",
+								"precio": 1000,
+								"cantidad":3
+							},
+							{
+								"nombre": "Helado de Coco",
+								"tipoPresentacion": "Cono",
+								"sabor":[
+									{"nombre": "Coco"},
+									{"nombre": "Caramelo"}
+								],
+								"azucar": 300,
+								"numeroLote": 3,
+								"fechaFabricacion": "2022-09-08",
+								"precio": 500,
+								"cantidad":5
+							}
+						]
+					}',1,
+					1);
+
 */
+
--- procedimientos.sql
+++ procedimientos.sql
@@ -1,4 +1,8 @@
-
+/*
+Tarea JSON || Bases de Datos II
+Por: Felipe Obando & Sebastián Bermúdez
+12 de septiembre del 2022
+*/
 
 -- 1. Cree un procedimiento para generar la compra de productos para un cliente 
 CREATE OR REPLACE PROCEDURE public.compraproducto(
@@ -51,50 +55,6 @@ AS $BODY$
     
 END $BODY$;
 
-CALL CompraProducto('{ "productos":[
-							{
-								"nombre": "Helado de Fresa",
-								"tipoPresentacion": "Cono",
-								"sabor":[
-									{"nombre": "Fresa"},
-									{"nombre": "Vainilla"}
-								],
-								"azucar": 100,
-								"numeroLote": 1,
-								"fechaFabricacion": "2022-09-08",
-								"precio": 600,
-								"cantidad":2
-							},
-							{
-								"nombre": "Helado Chocolate",
-								"tipoPresentacion": "Paleta",
-								"sabor":[
-									{"nombre": "Chocolate"},
-									{"nombre": "Oreo"}
-								],
-								"azucar": 500,
-								"numeroLote": 2,
-								"fechaFabricacion": "2022-09-08",
-								"precio": 1000,
-								"cantidad":3
-							},
-							{
-								"nombre": "Helado de Coco",
-								"tipoPresentacion": "Cono",
-								"sabor":[
-									{"nombre": "Coco"},
-									{"nombre": "Caramelo"}
-								],
-								"azucar": 300,
-								"numeroLote": 3,
-								"fechaFabricacion": "2022-09-08",
-								"precio": 500,
-								"cantidad":5
-							}
-						]
-					}',1,1);
-
-
 -- 2. Cree un procedimiento que genere el pedido personalizado de un cliente 
 CREATE OR REPLACE PROCEDURE generarPedido(idEmpleadoIN integer,idClienteIN integer,infoHelado json)
 
@@ -145,24 +105,150 @@ begin
 
 end;$generarPedido$
 
-select * from Empleado;
-select * from Cliente;
-
-call generarPedido(1,1,'{ 
-				   "nombre": "Helado hechizo 3",
-					"tipoPresentacion": "cono",
-					"sabor":[
-						{"nombre": "cacao"},
-						{"nombre": "guacamole"}
-						] }');
-						  
-select * from producto;
-
-
 
 -- 3. Cree un procedimiento que busque productos por alguna de las características, obtenga el 
 --precio y cantidades disponibles 
+CREATE OR REPLACE FUNCTION productosXnombre(
+	nombre_ character varying
+)
+RETURNS setof busquedaProductos
+LANGUAGE plpgsql as $pX$
 
+BEGIN
+	--El ingreso nulo del parámetro lo valida postgres, no permite llamar la función 
+	if (select count(idproducto) from Producto where infoProducto->>'nombre'=nombre_)=0 then
+		raise notice 'No existe coincidencias de producto con el nombre ingresado';
+	else
+		return query 
+		select infoProducto->>'precio' as precio, cantidad from Producto
+		where infoproducto->>'nombre' = nombre_;
+	end if;
+
+
+END; $pX$
+
+CREATE OR REPLACE FUNCTION productosXtipoPresentacion(
+	tipoPresentacion_ character varying
+)
+RETURNS setof busquedaProductos
+LANGUAGE plpgsql as $pXtp$
+
+BEGIN
+	--El ingreso nulo del parámetro lo valida postgres, no permite llamar la función 
+	if (select count(idproducto) from Producto where infoProducto->>'tipoPresentacion'=tipoPresentacion_)=0 then
+		raise notice 'No existe coincidencias de producto con el tipo de presentacion ingresado';
+	else
+		return query 
+		select infoProducto->>'precio' as precio, cantidad from Producto
+		where infoproducto->>'tipoPresentacion' = tipoPresentacion_;
+	end if;
+
+
+	end; $pXtp$
+
+CREATE OR REPLACE FUNCTION productosXnumeroLote(
+	numeroLote_ character varying
+)
+RETURNS setof busquedaProductos
+LANGUAGE plpgsql as $pX$
+
+BEGIN
+	--El ingreso nulo del parámetro lo valida postgres, no permite llamar la función 
+	if (select count(idproducto) from Producto where infoProducto->>'numeroLote'=numeroLote_)=0 then
+		raise notice 'No existe coincidencias de producto con el numero de lote ingresado';
+	else
+		return query 
+		select infoProducto->>'precio' as precio, cantidad from Producto
+		where infoproducto->>'numeroLote' = numeroLote_;
+	end if;
+
+
+END; $pX$
+
+CREATE OR REPLACE FUNCTION productosXfechaFabricacion(
+	fechaFabricacion_ character varying
+)
+RETURNS setof busquedaProductos
+LANGUAGE plpgsql as $pX$
+
+BEGIN
+	--El ingreso nulo del parámetro lo valida postgres, no permite llamar la función 
+	if (select count(idproducto) from Producto where infoProducto->>'fechaFabricacion'=fechaFabricacion_)=0 then
+		raise notice 'No existe coincidencias de producto con el nombre ingresado';
+	else
+		return query 
+		select infoProducto->>'precio' as precio, cantidad from Producto
+		where infoproducto->>'fechaFabricacion' = fechaFabricacion_;
+	end if;
+
+
+END; $pX$
 -- 4. Cree un procedimiento que obtenga los montos de ventas por productos, recibe como 
 --parámetro fechas (opcionales), cliente(opcionales), producto(opcional) 
- 
+CREATE OR REPLACE FUNCTION montosVentaXProducto(
+	fecha_ DATE DEFAULT NULL,
+	idCliente_ INTEGER DEFAULT NULL,
+	nombreProducto_ character varying DEFAULT NULL
+)
+RETURNS integer
+LANGUAGE plpgsql as $mvXp$
+DECLARE
+	montoFinal integer;
+	
+BEGIN
+	CREATE TEMP TABLE productosVendidos AS
+		SELECT fecha, nombre, precio, cantidad FROM (
+			SELECT 
+			(json_array_elements(productos->'productos')->>'fechaFabricacion')::date as fecha,
+			(json_array_elements(productos->'productos')->>'nombre')::character varying as nombre,
+			(json_array_elements(productos->'productos')->>'precio')::integer as precio,
+			(json_array_elements(productos->'productos')->>'cantidad')::integer as cantidad
+			from Venta
+		) as infoProductos;
+		
+	IF (fecha_ is null --con nada
+		and idCliente_ is null
+		and nombreProducto_ is null	
+		) then
+		select sum(precio*cantidad) from productosVendidos into montoFinal;
+	
+	ELSIF (fecha_ is not null  --con fecha
+		and idCliente_ is null
+		and nombreProducto_ is null	
+		) then
+			
+			IF (fecha_ > NOW()) then
+				raise notice 'La fecha no es válida';
+				select 0 into montoFinal;
+			ELSE
+				select sum(precio*cantidad) from productosVendidos into montoFinal where fecha = fecha_;
+			END IF;
+		
+	ELSIF (fecha_ is null  --con idCliente
+		and idCliente_ is not null
+		and nombreProducto_ is null	
+		) then
+			IF (select count(idCliente) from Cliente where idCliente=idCliente_)=0 then--no existe el cliente
+				raise notice 'No existe el cliente ingresado';
+				select 0 into montoFinal;
+			ELSE
+				select sum(precios) from 
+					(select (json_array_elements(productos->'productos')->>'precio')::integer 
+					 as precios from Venta where idCliente = 1)
+				as preciosTotales into montoFinal;
+			END IF;
+		
+	ELSIF (fecha_ is null  --con nombreProducto
+		and idCliente_ is null
+		and nombreProducto_ is not null	
+		) then
+			select sum(precio*cantidad) from productosVendidos into montoFinal where nombre = nombreProducto_;
+	
+	END IF;
+		
+		drop table productosVendidos;
+		
+		return montoFinal;
+	
+	
+END; $mvXp$
